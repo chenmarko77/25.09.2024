b7b1182649ab6bc117ad2ec9364a6bd643b7bc40
markochen <chenmarko77@gmail.com>
2024-09-27 22:52:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,111 @@
+// let i = 0
+// while (i<50) {
+//     i++
+//     console.log(`ЦЕ моє ${i} віджиманя`)
+//   }
+
+// let password = '';
+// do {
+//   password = prompt('Введіть пароль довше 4-х символів', '');
+// } while (password.length < 5);
+
+
+// console.log('Ввели пароль: ', password);
+
+// let i = 0
+
+// do {
+//     i++
+//       console.log(`ЦЕ моє ${i} віджиманя`)
+//   }  while (i<50) 
+
+      
+    // for (let index = 1; index <= 50; index++) {
+    //     console.log(`ЦЕ мій ${index} 00$`)
+
+    // }
+
+    // for (let Math = 0; Math < 11; Math++) {
+    //     console.log(`5 * ${Math} = ${ 5 * Math}`);    
+    // }
+
+    // for (let i = 1; i <= 50; i += 1) {
+    //     console.log(`Це моє ${i} віджимання`)
+    //     if (i === 25) {
+    //         break;
+    //     }
+    // }
+
+    // const number = 20;
+    // for (let i = 1; i < number; i += 1) {
+    //   if (i === 6) {
+    //     continue;
+    //   }
+    //   else if (i === 11) {
+    //     continue;
+    //   }
+    //   else if (i === 19) {
+    //     continue;
+    //   }
+    //   console.log(`Це моє ${i} віджимання`);
+    // }
+
+
+
+
+
+
+
+
+//     let i = 0
+// while (i<5) {
+//     i++
+//     console.log(`ЦЕ моє ${i} віджиманя`)
+//   }
+
+// let i = 0
+
+// do {
+//     i++
+//       console.log(`ЦЕ моє ${i} віджиманя`)
+//   }  while (i<5) 
+
+// for (let number = 1; number < 6; number++) {
+//     console.log(`Число ${number}`);
+// }
+
+// let num = 3;
+//         while (num) {
+//             console.log(num);
+//             num -= 1;
+//         }
+
+        // for (let number = 0; number < 5; number++) {
+        //     console.log(`Число ${number}`);
+        // }
+
+//         let number = 0
+// while (number<5) {
+//     number++
+//     console.log(`Число ${number}`)
+//   }
+
+
+let s = Number(prompt('enter average score'))
+
+// for (let index = 0; index < 1000000; index++) {
+// let s = Number(prompt('enter average score'))
+// if (s < 7) {
+//     break
+// } 
+// }
+
+
+
+for (let index = 0; index < 1000; index++) {
+    if (index % 2 !== 0) {
+        console.log(index);
+        
+    }
+    
+}
